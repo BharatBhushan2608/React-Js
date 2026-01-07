755f3fe1ed30f7bff24e9b7b38b0bbbe46135c0d
BharatBhushan2608 <e22cseu1666@bennett.edu.in>
2026-01-07 22:55:05 +0530

    react router dom

---
+++ 18-react-router-dom/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import NavBar from './pages/components/NavBar.jsx'
+import Product from './pages/Product.jsx'
+
+ 
+ const App = () => {
+   return (
+     <div>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/about' element={ <About />} />
+        <Route path='/contact' element={ <Contact />} />
+        <Route path='/product' element={ <Product /> } />
+      </Routes>
+     </div>
+   )
+ }
+ 
+ export default App
---
+++ 18-react-router-dom/src/pages/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    
+    <div className='nav'>
+        <h2> Bharat </h2>
+        <div>
+            <Link to='/'>Home</Link>|
+            <Link to='/about'>About</Link>|
+            <Link to='/contact'>Contact</Link>|
+            <Link to='/product'>Product</Link>
+           
+        </div>
+      </div>
+  )
+}
+
+export default NavBar
